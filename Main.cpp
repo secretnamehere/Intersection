2348a73f06a4563c98a29827d682773f0b8d3b96
secretnamehere <73169968+secretnamehere@users.noreply.github.com>
2021-12-18 21:18:48 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
 	Vector3D a(0, 0, 0);
-	Vector3D b(1, 1, 1);
-	Vector3D c(0.5, -0.5, 0.5);
-	Vector3D d(0.5, 14, 0.5);
+	Vector3D b(1, 1, -25);
+	Vector3D c(-1, 1, -5);
+	Vector3D d(3, -2, 0);
 	
 	Segment3D segment1(a, b);
 	Segment3D segment2(c, d);
